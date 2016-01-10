66debc6c94a0e4f62063713cd689394d8f74807f
Auke van Slooten <auke@muze.nl>
2016-01-10 19:40:24 +0100

    added documentation and docblocks

--- src/prototype.php
+++ src/prototype.php
@@ -14,21 +14,50 @@ namespace arc;
  * Experimental functionality, may be removed later, use at own risk.
  * @package arc
  */
-final class prototype {
+final class Prototype 
+{
 
+	/**
+	 * @var \SplObjectStorage contains a list of frozen objects and the observer
+	 */
 	private static $frozen = null;
 
+	/**
+	 * @var \SplObjectStorage contains a list of objects made unextensible and the observer
+	 */
 	private static $notExtensible = null;
 
+	/**
+	 * @var \SplObjectStorage contains a list of all 'child' instances for each prototype
+	 */
 	private static $instances = null;
 
+	/**
+	 * @var \SplObjectStorage contains a list of all observers for each prototype
+	 */
 	private static $observers = null;
 
-	public static function create($properties) {
+	/**
+	 * Returns a new \arc\prototype\Prototype object with the given properties. The 
+	 * properties array may contain closures, these will be available as methods on 
+	 * the new Prototype object.
+	 * @param array $properties List of properties and methods
+	 * @return \arc\prototype\Prototype
+	 */
+	public static function create($properties) 
+	{
 		return new prototype\Prototype($properties);
 	}
 
-	public static function extend($prototype, $properties) {
+	/**
+	 * Returns a new \arc\prototype\Prototype object with the given object as its
+	 * prototype and the given properties and methods set.
+	 * @param \arc\prototype\Prototype $prototype The prototype for this object
+	 * @param array $properties List of properties and methods
+	 * @return \arc\prototype\Prototype
+	 */
+	public static function extend($prototype, $properties) 
+	{
 		if ( self::isExtensible($prototype) ) {
 			if (!isset(self::$instances)) {
 				self::$instances = new \SplObjectStorage();
@@ -47,7 +76,12 @@ final class prototype {
 		}
 	}
 
-	public static function _destroy($obj) {
+	/**
+	 * Helper method to remove cache information when a prototype is no longer needed.
+	 * @param \arc\prototype\Prototype $obj The object to be removed
+	 */
+	public static function _destroy($obj) 
+	{
 		self::unfreeze($obj);
 		if ( isset(self::$notExtensible[$obj]) ) {
 			unset(self::$notExtensible[$obj]);
@@ -60,7 +94,16 @@ final class prototype {
 		}
 	}
 
-	public static function assign($prototype) {
+	/**
+	 * Returns a new \arc\prototype\Prototype with the given prototype set. In addition 
+	 * all properties on the extra objects passed to this method will be copied to the 
+	 * new Prototype object. For any property that is set on multiple objects, the value 
+	 * of the property in the later object overwrites values from other objects.
+	 * @param \arc\prototype\Prototype $prototype the prototype for the new object
+	 * @param \arc\prototype\Prototype ...$object the objects whose properties will be assigned
+	 */
+	public static function assign($prototype) 
+	{
 		$objects = func_get_args();
 		array_shift($objects);
 		$properties = [];
@@ -70,7 +113,14 @@ final class prototype {
 		return self::extend($prototype, $properties);
 	}
 
-	public static function freeze($prototype) {
+	/**
+	 * This makes changes to the given Prototype object impossible, untill you call 
+	 * \arc\prototype::unfreeze($prototype). The object becomes immutable. Any 
+	 * attempt to change the object will silently fail.
+	 * @param \arc\prototype\Prototype $prototype the object to freeze
+	 */
+	public static function freeze($prototype) 
+	{
 		if (!isset(self::$frozen)) {
 			self::$frozen = new \SplObjectStorage();
 		}
@@ -80,63 +130,146 @@ final class prototype {
 		return self::observe($prototype, self::$frozen[$prototype]);	
 	}
 
-	public static function unfreeze($prototype) {
+	/**
+	 * Makes the given object mutable again.
+	 * @param \arc\prototype\Prototype $prototype
+	 */
+	public static function unfreeze($prototype) 
+	{
 		if ( isset(self::$frozen) && isset(self::$frozen[$prototype]) ) {
 			self::unobserve($prototype, self::$frozen[$prototype]);
 			unset(self::$frozen[$prototype]);
 		}
 	}
 
-	public static function keys($prototype) {
+	/**
+	 * Returns a list of keys of all the properties in the given prototype
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function keys($prototype) 
+	{
 		$entries = static::entries($prototype);
 		return array_keys($entries);
 	}
 
-	public static function entries($prototype) {
+	/**
+	 * Returns an array with key:value pairs for all properties in the given prototype
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function entries($prototype) 
+	{
 		return $prototype->properties;
 	}
 
-	public static function values($prototype) {
+	/**
+	 * Returns a list of all the property values in the given prototype
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function values($prototype) 
+	{
 		$entries = static::entries($prototype);
 		return array_values($entries);
 	}
 
-	public static function hasProperty($prototype, $property) {
+	/**
+	 * Returns true if the the property name is available in this prototype
+	 * @param \arc\prototype\Prototype $prototype
+	 * @param string $property
+	 * @return bool
+	 */
+	public static function hasProperty($prototype, $property) 
+	{
 		$entries = static::entries($prototype);
 		return array_key_exists($property, $entries);
 	}
 
-	public static function ownKeys($prototype) {
+	/**
+	 * Returns a list of all the property names defined in this prototype instance
+	 * without traversing its prototypes.
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function ownKeys($prototype) 
+	{
 		$entries = static::ownEntries($prototype);
 		return array_keys($entries);
 	}
 
-	public static function ownEntries($prototype) {
+	/**
+	 * Returns an array with key:value pairs for all properties in this prototype
+	 * instance wihtout traversing its prototypes.
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function ownEntries($prototype) 
+	{
+		// this needs access to the private _ownProperties variable
+		// this is one way to do that.
 		$f = \Closure::bind(function() use ($prototype) {
 			return $prototype->_ownProperties;
 		}, $prototype, $prototype);
 		return $f();
 	}
 
-	public static function ownValues($prototype) {
+	/**
+	 * Returns a list of all the property values in the given prototype
+	 * instance wihtout traversing its prototypes.
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function ownValues($prototype) 
+	{
 		$entries = static::ownEntries($prototype);
 		return array_values($entries);
 	}
 
-	public static function hasOwnProperty($prototype, $property) {
+	/**
+	 * Returns true if the the property name is available in this prototype
+	 * instance wihtout traversing its prototypes.
+	 * @param \arc\prototype\Prototype $prototype
+	 * @param string $property
+	 * @return bool
+	 */
+	public static function hasOwnProperty($prototype, $property) 
+	{
 		$entries = static::ownEntries($prototype);
 		return array_key_exists($property, $entries);
 	}
 
-	public static function isFrozen($prototype) {
+	/**
+	 * Returns true if the given prototype is made immutable by freeze()
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return bool
+	 */
+	public static function isFrozen($prototype) 
+	{
 		return isset(self::$frozen[$prototype]);
 	}
 
-	public static function isExtensible($prototype) {
+	/**
+	 * Returns true if the given prototype is made not Extensible
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return bool
+	 */
+	public static function isExtensible($prototype) 
+	{
 		return !isset(self::$notExtensible[$prototype]);
 	}
 
-	public static function observe($prototype, $callback) {
+	/**
+	 * This calls the $callback function each time a property of $prototype is 
+	 * changed or unset. The callback is called with the prototype object, the 
+	 * name of the property and the new value (null if unset).
+	 * If the closure returns false exactly (no other 'falsy' values will work), 
+	 * the change will be cancelled
+	 * @param \arc\prototype\Prototype $prototype
+	 * @param \Closure $callback
+	 */
+	public static function observe($prototype, $callback) 
+	{
 		if ( !isset(self::$observers) ) {
 			self::$observers = new \SplObjectStorage();
 		}
@@ -146,35 +279,64 @@ final class prototype {
 		self::$observers[$prototype][$callback] = true;
 	}
 
-	public static function getObservers($prototype) {
+	/**
+	 * Returns a list of observers for the given prototype.
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function getObservers($prototype) 
+	{
 		return (isset(self::$observers[$prototype]) ? self::$observers[$prototype] : [] );
 	}
 
-	public static function preventExtensions($prototype) {
+	/**
+	 * Makes an object no longer extensible.
+	 * @param \arc\prototype\Prototype $prototype
+	 */
+	public static function preventExtensions($prototype) 
+	{
 		if ( !isset(self::$notExtensible) ) {
 			self::$notExtensible = new \SplObjectStorage();
 		}
 		self::$notExtensible[$prototype] = true;
 	}
 
-	public static function unobserve($prototype, $callback) {
+	/**
+	 * Removes an observer callback for the given prototype.
+	 * @param \arc\prototype\Prototyp $prototype
+	 * @param \Closure $callback the observer callback to be removed
+	 */
+	public static function unobserve($prototype, $callback) 
+	{
 		if ( isset(self::$observers) && isset(self::$observers[$prototype]) ) {
 			unset(self::$observers[$prototype][$callback]);
 		}
 	}
 
-	public static function hasPrototype($obj, $prototype) {
+	/**
+	 * Returns true if the object as the given prototype somewhere in its
+	 * prototype chain, including itself.
+	 */
+	public static function hasPrototype($obj, $prototype) 
+	{
         if (!$obj->prototype) {
             return false;
         }
-        if ($obj->prototype === $prototype) {
+        if ($obj === $prototype || $obj->prototype === $prototype) {
             return true;
         }
 
         return static::hasPrototype($obj->prototype, $prototype );
 	}
 
-	public static function getDescendants($prototype) {
+	/**
+	 * Returns a list of prototype objects that have this prototype object
+	 * in their prototype chain.
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function getDescendants($prototype) 
+	{
 		$instances = self::getInstances($prototype);
 		$descendants = $instances;
 		foreach ($instances as $instance) {
@@ -183,11 +345,24 @@ final class prototype {
 		return $descendants;
 	}
 
-	public static function getInstances($prototype) {
+	/**
+	 * Returns a list of prototype objects that have this prototype object
+	 * as their direct prototype.
+	 * @param \arc\prototype\Prototype $prototype
+	 * @return array
+	 */
+	public static function getInstances($prototype) 
+	{
 		return (isset(self::$instances[$prototype]) ? self::$instances[$prototype] : [] );
 	}
 
-	public static function getPrototypes($obj) {
+	/**
+	 * Returns the full prototype chain for the given object.
+	 * @param \arc\prototype\Prototype $obj
+	 * @return array
+	 */
+	public static function getPrototypes($obj) 
+	{
 		$prototypes = [];
 		while ( $prototype = $obj->prototype ) {
 			$prototypes[] = $prototype;
